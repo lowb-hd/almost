5197175fffe149382eba27f5600c760115a5f529
handong <510093396@qq.com>
2020-06-23 01:22:37 +0800

    good

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,8 +5,11 @@
 # @Description : 
 """定义learning_logs的URL模式"""
 from django.conf.urls import url
+from django.urls import path
 from . import views
 urlpatterns = [
     # 主页
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    path('han/',views.index)
+
 ]
